945ff19874249945fad9e232fce7a996d13ea717
indprtech <indprtech@gmail.com>
2024-02-21 00:57:35 +0530

    Add Lists

---
+++ cards/list.dart
@@ -0,0 +1,38 @@
+import 'package:example_ui_elements/uiElements/utils/SimpleText.dart';
+import 'package:flutter/material.dart';
+
+class UIList {
+  Widget newList(
+      {required String label,
+      IconData icon = Icons.android,
+      Color backgorund = Colors.white,
+      required Widget child,
+      required Function onTap,
+      bool enableIcon = false}) {
+    return GestureDetector(
+      onTap: () => onTap(),
+      child: Container(
+        decoration: BoxDecoration(
+            color: backgorund, borderRadius: BorderRadius.circular(8.0)),
+        child: Row(
+          children: [
+            enableIcon ? Icon(icon) : const SizedBox(),
+            enableIcon
+                ? const SizedBox(
+                    width: 10,
+                  )
+                : const SizedBox(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SimpleText().newText(label, weight: FontWeight.w800),
+                child
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
